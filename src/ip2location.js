10106420a10418b09ecd2016d006753d72c763c5
ooi18 <ooi18@protonmail.com>
2024-04-03 15:32:06 +0800

    Update ip2location.js

--- src/ip2location.js
+++ src/ip2location.js
@@ -323,7 +323,7 @@ export class IP2Location {
 	  // console.log(buffer);
 	  let var1 = buffer.readUInt32LE(position);
 	  console.log("var1: " + var1);
-	  console.log("position: " + position);
+	  // console.log("position: " + position);
     // return buffer.readUInt32LE(position);
     return var1;
   }
